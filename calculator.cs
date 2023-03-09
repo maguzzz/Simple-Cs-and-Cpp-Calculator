8640aadab7d49faaad1b4ca46544e7a6c642c812
Markus <65002100+maguzzz@users.noreply.github.com>
2023-03-09 13:19:49 +0100

    First simple C# Calculator
    A Calculator where you can continue to calculate with the last some

---
+++ calculator.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+
+string option = "";
+do
+{
+    Console.Write("First number | ");
+    double num1 = Convert.ToDouble(Console.ReadLine());
+    do
+    {
+        Console.Write("+   -  *  /  % | ");
+        string zeichen = Console.ReadLine();
+        Console.Write("Second number | ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        switch (zeichen)
+        {
+            case "+":
+                num1 += num2;
+                break;
+            case "-":
+                num1 -= num2;
+                break;
+            case "*":
+                num1 *= num2;
+                break;
+            case "/":
+                num1 /= num2;
+                break;
+            case "%":
+                num1 %= num2;
+                break;
+            default:
+                Console.WriteLine("ERROR");
+                break;
+        }
+        Console.WriteLine(num1);
+
+        Console.WriteLine("(E)xit | (C)alc more | (R)estart");
+        option = Console.ReadLine();
+    }
+    while (option == "C" || option == "C");
+}
+while (option == "R" || option == "R");
